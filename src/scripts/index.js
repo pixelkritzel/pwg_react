e7af932114066034ce07ae3249fb6cd7dc8b7e72
Timo Zoeller <timo@pixelkritzel.de>
2016-08-31 20:00:22 +0200

    Migrates project to authors own gulp boilerplate (https://github.com/pixelkritzel/gulp_es6_sass_boilerplate)

--- src/scripts/index.js
+++ src/scripts/index.js
@@ -2,6 +2,4 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import { Pwg } from './Pwg';
 
-import '!style!css!sass!./scss/style.scss';
-
 ReactDOM.render(<Pwg />, document.getElementById('root'));
