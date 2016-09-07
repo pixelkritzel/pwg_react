2d82e4e2b8f61a25c50b224f1ae211810492910f
Timo Zoeller <timo@pixelkritzel.de>
2016-09-07 17:32:02 +0200

    Improves error message for too short salt

--- src/scripts/generatePassword.js
+++ src/scripts/generatePassword.js
@@ -6,8 +6,8 @@ var generatePassword = function(serviceName, salt) {
   if(!serviceName) {
     throw new Error('Servicename is missing');
   }
-  if(salt.length < 6) {
-    throw new Error('Salt is too short.')
+  if(salt.length < 5) {
+    throw new Error('Salt is too short. It must be atleast 6 characters');
   }
   var copwd = serviceName.trim() + salt.trim();
   var password = btoa(CryptoJS.SHA1(copwd)).substr(0,32);
